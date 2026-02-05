d77bc3451d27b7a523e59ab396b8ec1f1f068d3a
sharedlopezserrano <slopezserrano@byupathway.edu>
2026-02-05 19:50:44 -0300

    W04 Learning Activity Form Sign Up and Registration with Cliente Side Data and Server Side Data

--- server.js
+++ server.js
@@ -12,8 +12,38 @@ const app = express()
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
-
 const static = require("./routes/static")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
+
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
 
 /* ***********************
  View Engine and Templates
@@ -28,10 +58,9 @@ app.set("layout", "./layouts/layout") // not at views root
 
 // Index route
 app.get("/", utilities.handleErrors(baseController.buildHome))
-
-app.use(static)
 app.use("/inv", inventoryRoute)
-
+app.use("/account", accountRoute)
+app.use(static)
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
